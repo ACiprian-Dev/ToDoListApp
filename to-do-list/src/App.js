86786714dcd867aff8291e599717aaf5963b1345
Ciprian <avadaneiciprian.dev@gmail.com>
2022-03-09 22:56:10 +0200

    update

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -30,22 +30,26 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Pokedex</h1>
-      <NavigationComponent></NavigationComponent>
-      <section className="cardGrid">
-        <CardComponent name = "bulbasaur" id = "1" type = "grass" ability = "poison" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></CardComponent>
-        <CardComponent name = "ivysaur" id = "2" type = "grass" ability = "poison" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"></CardComponent>
-        <CardComponent name = "venusaur" id = "3" type = "grass" ability = "poison" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"></CardComponent>
-        <CardComponent name = "charmander" id = "4" type = "fire" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"></CardComponent>
-        <CardComponent name = "charmeleon" id = "5" type = "fire" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"></CardComponent>
-        <CardComponent name = "Charizard" id = "6" type = "fire" ability = "flying" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"></CardComponent>
-        <CardComponent name = "squirtle" id = "7" type = "water" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"></CardComponent>
-        <CardComponent name = "Wartortle" id = "8" type = "water" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png"></CardComponent>
-        <CardComponent name = "Blastoise" id = "9" type = "water" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png"></CardComponent>
-        <CardComponent name = "Pikachu" id = "25" type = "electric" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"></CardComponent>
-        <CardComponent name = "Eevee" id = "133" type = "normal" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/133.png"></CardComponent>
-        <CardComponent name = "Gengar" id = "94" type = "ghost" ability = "poison" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png"></CardComponent>
-      </section>
+      <div className="container">
+        <div className="header">
+          <h1>Pokedex</h1>
+          <NavigationComponent></NavigationComponent>
+        </div>
+        <section className="cardGrid">
+          <CardComponent name = "bulbasaur" id = "1" type = "grass" ability = "poison" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></CardComponent>
+          <CardComponent name = "ivysaur" id = "2" type = "grass" ability = "poison" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"></CardComponent>
+          <CardComponent name = "venusaur" id = "3" type = "grass" ability = "poison" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"></CardComponent>
+          <CardComponent name = "charmander" id = "4" type = "fire" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"></CardComponent>
+          <CardComponent name = "charmeleon" id = "5" type = "fire" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"></CardComponent>
+          <CardComponent name = "Charizard" id = "6" type = "fire" ability = "flying" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"></CardComponent>
+          <CardComponent name = "squirtle" id = "7" type = "water" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"></CardComponent>
+          <CardComponent name = "Wartortle" id = "8" type = "water" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png"></CardComponent>
+          <CardComponent name = "Blastoise" id = "9" type = "water" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png"></CardComponent>
+          <CardComponent name = "Pikachu" id = "25" type = "electric" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"></CardComponent>
+          <CardComponent name = "Eevee" id = "133" type = "normal" ability = "" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/133.png"></CardComponent>
+          <CardComponent name = "Gengar" id = "94" type = "ghost" ability = "poison" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png"></CardComponent>
+        </section>
+      </div>
     </div>
   );
 }
